c724fd41888700fd0cd7fdafc96383d396180891
Winston Limanto <wmlimanto@gmail.com>
2022-03-05 20:37:49 -0800

    add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -21,6 +21,8 @@ if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
 
+const raining = true;
+
 if (!raining) {
-  console.log("Leave your umbrella at home!!");
+  console.log("Leave your umbrella at home!");
 }
