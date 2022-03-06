bd6f522e63900077c9202115de759f2afb14e8bd
Winston Limanto <wmlimanto@gmail.com>
2022-03-05 20:56:46 -0800

    added which school practice work

---
+++ which-school.js
@@ -0,0 +1,9 @@
+const whichSchool  = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (13 <= age && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+  }
