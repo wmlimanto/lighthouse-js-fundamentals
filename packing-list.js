2e41bd846211b4db5871850acd2c791543f1b11b
Winston Limanto <wmlimanto@gmail.com>
2022-03-07 19:42:24 -0800

    javascript practice with basic arrays and for loops

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]); //declaring variable i (index) in this loop until it matches the length of our array so it will list all the items
+}
+
